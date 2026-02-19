1d6147d539cb62fcc09bb99b9a0357c523870ff7
Baikhonov Samrat <baikhonov.samrat@gmail.com>
2026-02-19 13:11:33 +0500

    Прошёл главу Работа с классами и стилями

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
-const count = ref(2);
+const isActive = ref(true);
+const activeClass = ref('active');
+const errorClass = ref('text-danger');
 
-const alwaysSmall = computed({
-  get(previous) {
-    if (count.value <= 3) {
-      return count.value;
-    }
+const activeColor = ref('red');
+const fontSize = ref(30);
 
-    return previous;
-  },
-  set(newValue) {
-    count.value = newValue * 2;
-  },
+const styleObject = reactive({
+  color: 'green',
+  fontSize: '24px',
 });
 </script>
 
 <template>
-  <div>{{ alwaysSmall }}</div>
+  <div :style="styleObject">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui illum
+    asperiores non excepturi tempore vero, aliquid nisi quidem soluta delectus
+    nostrum expedita corporis ipsum harum ducimus nulla. Mollitia, libero
+    distinctio!
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.static {
+  font-weight: bold;
+}
+
+.active {
+  width: fit-content;
+  padding: 10px;
+
+  background: orange;
+}
+
+.text-danger {
+  color: white;
+  background: rgb(221, 4, 4);
+}
+
+.info {
+  font-style: italic;
+}
+</style>
